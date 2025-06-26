dbd92cf42bc1b7f4dbd132320a7a70eced9dbfda
midhun-dh <midhun.m@enxcl.com>
2025-06-26 12:50:33 +0530

    services

---
+++ Services/ICompanyService.cs
@@ -0,0 +1,9 @@
+﻿using EchoCart_Backend.Models.Responses;
+
+namespace EchoCart_Backend.Services
+{
+    public interface ICompanyService
+    {
+        public GetAllCompaniesResponse GetCompanies(int TenantID);
+    }
+}
